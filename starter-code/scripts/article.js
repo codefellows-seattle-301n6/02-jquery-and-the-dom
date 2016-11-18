2b0a38a03aa19de1193ba21a1e00b336e427dbb2
Rosemary Tuttle <rosemary.brinton@gmail.com>
2016-11-17 19:55:15 -0800

    Added newArticle, trying to add body

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -15,13 +15,12 @@ function Article (opts) {
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
-  $newArticle.attr('data-title', this.title);
+  $newArticle.find('h1').text(this.title);
   $newArticle.attr('data-category', this.category);
-  $newArticle.attr('data-author', this.author);
-  $newArticle.attr('data-authorUrl', this.authorUrl)
-  $newArticle.attr('data-publishedOn', this.publishedOn)
-  $newArticle.attr('data-body', this.body);
+  $newArticle.find('a').text(this.author);
+  $newArticle.find('a').attr('href', this.authorUrl);
 
+  $newArticle.hasClass('article-body').text(this.body);
   /* TODO: Now use jQuery to fill in the rest of the current
   template clone with properties from this particular Article instance.
   We need to fill in:
@@ -31,14 +30,15 @@ Article.prototype.toHtml = function() {
     4. article body, and
     5. publication date. */
     // Display the date as a relative number of 'days ago'
-    $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-    $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
 
     /* TODO: This cloned article is no longer a template,
     as it now has real data attached to it! We need to account
     for that before this current article gets rendered to our
     DOM. */
-    return $newArticle;
+  $newArticle.removeClass('template');
+  return $newArticle;
 };
 
 ourLocalData.sort(function(firstElement, secondElement) {
