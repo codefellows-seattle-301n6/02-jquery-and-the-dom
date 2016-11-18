17609ec3d6582065cadece8a114ea42b47665f39
Maithrika Sara <maithrika@Consciously-Uncoupling.local>
2016-11-17 19:57:43 -0800

    commit before pull

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -30,15 +30,16 @@ Article.prototype.toHtml = function() {
     3. article title,
     4. article body, and
     5. publication date. */
+
     // Display the date as a relative number of 'days ago'
-    $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
-    $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newArticle.find('time[pubdate]').attr('title', this.publishedOn);
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
 
     /* TODO: This cloned article is no longer a template,
     as it now has real data attached to it! We need to account
     for that before this current article gets rendered to our
     DOM. */
-    return $newArticle;
+  return $newArticle;
 };
 
 ourLocalData.sort(function(firstElement, secondElement) {
